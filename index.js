6b65101aa623c47866602136e40d750febb946cb
Thomas Bender <thomasmatthewbender@gmail.com>
2022-03-21 09:48:18 -0400

    commit tree 7fb6c4af60b489aeb074c67674d4f408c361a8e1

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+
+function multiply() {
+    return num1 * num2
+}
+
+console.log(multiply())
+
+let num3 = 24
+let num4 = 5
+
+
+
+console.log(multiply(31, 2))
+
+const random = () => {
+    return Math.random
+}
+
+const mod = (num3, num4) => {
+
+    return num3 % num4
+}
+
+const max = () => {
+    return Math.max
+}
